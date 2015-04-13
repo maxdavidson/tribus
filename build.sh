d980ea2367a7e06db8068b703838b67e1d5eabfa
Max Davidson <davidsonmax@gmail.com>
2015-04-13 21:12:51 +0200

    Removed distribution binary from source control

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@
 #npm install -g jspm
 #jspm install
 
+mkdir dist
+
 jspm bundle-sfx lib/extra/exporter dist/tribus.js
 jspm bundle-sfx lib/extra/exporter dist/tribus.min.js --minify --skip-source-maps
